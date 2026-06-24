172f49651ed012cd8128c92641e9fbcc74ca75f0
Isak Sky <607986+isaksky@users.noreply.github.com>
2026-06-23 18:49:40 -0600

    Initial commit

---
+++ vibeformer/src/vibeformer/vibeformer.clj
@@ -0,0 +1,6 @@
+(ns vibeformer.vibeformer)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (prn x "Hello, World!"))
---
+++ vibeformer/test/vibeformer/vibeformer_test.clj
@@ -0,0 +1,7 @@
+(ns vibeformer.vibeformer-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [vibeformer.vibeformer :as sut])) ; system under test
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
